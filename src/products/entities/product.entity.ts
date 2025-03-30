8b2d4f170e619f6d84bbef2d800c48f6cd2dffe8
AbrahamRivero <riveromartinezabraham@gmail.com>
2025-03-30 14:44:21 -0400

    add products module with CRUD operations and DTOs

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', { unique: true })
+  title: string;
+}
