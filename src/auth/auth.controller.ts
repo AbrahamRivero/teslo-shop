f7aebab13dbfa82bb7e0c4b74ad24ffc6d5ed98e
AbrahamRivero <riveromartinezabraham@gmail.com>
2025-04-11 16:42:39 -0400

    feat(auth, products, seed): enhance authentication and authorization features
    - Added checkAuthStatus method in AuthService to return user details and JWT token.
    - Introduced new private routes in AuthController with role-based access control.
    - Updated ProductsController to enforce role protection on create, update, and delete operations.
    - Integrated AuthModule into ProductsModule and SeedModule for consistent access control.
    - Enhanced SeedController to restrict seed execution to admin users only.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,15 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { Controller, Post, Body, Get, UseGuards, Req, Headers } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { GetUser } from './decorators/get-user.decorator';
 import { SignUpUserDto, SignInUserDto } from './dto';
 import { User } from './entities/user.entity';
+import { Auth, RawHeaders, RoleProtected } from './decorators';
+import { IncomingHttpHeaders } from 'http';
+import { ValidRoles } from './interfaces';
+import { UserRoleGuard } from './guards/user-role.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -20,11 +25,47 @@ export class AuthController {
     return this.authService.signIn(signInUserDto);
   }
 
+  @Get('check-status')
+  @Auth()
+  checkAuthStatus(@GetUser() user: User) {
+    return this.authService.checkAuthStatus(user);
+  }
+
   @Get('private')
   @UseGuards(AuthGuard())
-  testingPrivateRoute(@GetUser() user: User) {
+  testingPrivateRoute(
+    @Req() request: Express.Request,
+    @GetUser() user: User,
+    @GetUser('email') userEmail: string,
+
+    @RawHeaders() rawHeaders: string[],
+    @Headers() headers: IncomingHttpHeaders,
+  ) {
     return {
-      message: 'This is a private route',
+      ok: true,
+      message: 'Hola Mundo Private',
+      user,
+      userEmail,
+      rawHeaders,
+      headers,
+    };
+  }
+
+  @Get('private2')
+  @RoleProtected(ValidRoles.superUser, ValidRoles.admin)
+  @UseGuards(AuthGuard(), UserRoleGuard)
+  privateRoute2(@GetUser() user: User) {
+    return {
+      ok: true,
+      user,
+    };
+  }
+
+  @Get('private3')
+  @Auth(ValidRoles.admin)
+  privateRoute3(@GetUser() user: User) {
+    return {
+      ok: true,
       user,
     };
   }
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,6 +1,9 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AuthModule } from 'src/auth/auth.module';
+
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 
@@ -8,7 +11,7 @@ import { Product, ProductImage } from './entities';
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
-  imports: [TypeOrmModule.forFeature([Product, ProductImage])],
+  imports: [TypeOrmModule.forFeature([Product, ProductImage]), AuthModule],
   exports: [ProductsService, TypeOrmModule],
 })
 export class ProductsModule {}
