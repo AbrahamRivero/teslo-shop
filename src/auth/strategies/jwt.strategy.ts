c24a091096a0e54502a897d1da8dda962d73bc29
AbrahamRivero <riveromartinezabraham@gmail.com>
2025-04-11 11:44:28 -0400

    feat(auth): implement JWT authentication strategy and update dependencies
    - Added JwtModule and PassportModule to handle JWT authentication.
    - Created JwtStrategy for validating JWT tokens and user authentication.
    - Introduced JwtPayload interface for type safety in JWT payloads.
    - Updated package.json and yarn.lock to include necessary dependencies for JWT and Passport.
    - Enhanced AuthModule to export new modules and strategies for better modularity.

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,42 @@
+/* eslint-disable prettier/prettier */
+import { UnauthorizedException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PassportStrategy } from '@nestjs/passport';
+import { Repository } from 'typeorm';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
+
+import { User } from '../entities/user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+
+    private readonly configService: ConfigService,
+  ) {
+    const jwtSecret = configService.get<string>('JWT_SECRET');
+    if (!jwtSecret) {
+      throw new Error('JWT_SECRET must be defined');
+    }
+
+    super({
+      secretOrKey: jwtSecret,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const { email } = payload;
+    const user = await this.userRepository.findOneBy({ email });
+    if (!user) throw new UnauthorizedException('Token not valid');
+
+    if (!user.isActive)
+      throw new UnauthorizedException('User is not active, talk with admin');
+
+    return user;
+  }
+}
