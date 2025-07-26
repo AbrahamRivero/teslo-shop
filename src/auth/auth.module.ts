df94df43010c616c73f486632e2b82365471b501
AbrahamRivero <riveromartinezabraham@gmail.com>
2025-07-26 17:06:38 -0400

    fix(auth): reduce JWT expiration time for enhanced security
    - Updated the JWT signOptions in auth.module.ts to decrease the expiration time from 1 hour to 15 minutes. This change aims to improve security by limiting the duration of token validity.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -24,7 +24,7 @@ import { User } from './entities/user.entity';
       useFactory: (configService: ConfigService) => {
         return {
           secret: configService.get('JWT_SECRET'),
-          signOptions: { expiresIn: '1h' },
+          signOptions: { expiresIn: '15m' },
         };
       },
     }),
